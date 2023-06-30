12a8b6b5990944b284672a0a96319fa6e1d7ddbb
raihan1233 <milaniarahma123@gmail.com>
2023-06-30 11:37:26 +0700

    revisi

--- plantae/src/components/modules/Admin/components/ButtonImages.js
+++ plantae/src/components/modules/Admin/components/ButtonImages.js
@@ -3,15 +3,17 @@ import * as tf from '@tensorflow/tfjs';
 import * as mobilenet from '@tensorflow-models/mobilenet';
 
 import './style.css'
-import DataBunga from '../DataBunga'
+// import DataBunga from '../DataBunga'
 import FormPost from './FormPost';
-import { Alert } from 'react-bootstrap';
+import AlertErrorImages from '../../../layouts/components/alerts/AlertErrorImages';
 
 const ButtonImages = () => {
     const [isModelLoading, setIsModelLoading] = useState(false);
     const [model, setModel] = useState(null);
     const [imgURL, setImageURL] = useState(null)
     const [results, setResults] = useState([]);
+    const [showAlert, setShowAlert] = useState(false);
+    const [data, setData] = useState([]);
 
     const imageRef = useRef();
     const textInputRef = useRef();
@@ -58,61 +60,72 @@ const ButtonImages = () => {
     useEffect(() => {
         // Memuat model TensorFlow.js ketika komponen dipasang
         loadModel();
+
+        setShowAlert(true);
     }, []);
+
+    useEffect(() => {
+        fetchData();
+    }, [])
 
     if (isModelLoading) {
         return <h2>Model Loading...</h2>
     }
 
-    const AvailableProduct = DataBunga;
-    console.log(AvailableProduct);
+    const fetchData = async () => {
+        try {
+            const response = await fetch('http://localhost:3001/available-product');
+            const jsonData = await response.json();
+            setData(jsonData);
+        } catch (error) {
+            console.log(error);
+        }
+    };
 
     const currentFlower = results[0] && results[0].className;
     console.log(currentFlower);
 
-    const isexist = AvailableProduct.filter((item) => item.nama === currentFlower);
+    const isexist = data.filter((item) => item.name === currentFlower);
     if(Object.values(isexist).length > 0) {
         console.log('true');
-        return <FormPost />
+            return <>{ imgURL && <FormPost imgURL={imgURL} />}</>
     } else {
-        <Alert variant='danger'>
-            sorry this image is unavailable, please choose another image
-        </Alert>
         console.log('false');
+        return (
+            <>
+                <div className="App">
+                    <div className="inputHolder">
+                        <input type='file' accept='image/*' capture='camera' className='uploadInput' onChange={uploadImage} ref={fileInputRef} />
+                        <button className='uploadImage' onClick={triggerUpload}>Upload Image</button>
+                        <span className="or">OR</span>
+                        <input type="text" placeholder='Enter URL image...' ref={textInputRef} onChange={handleOnChange} />
+                    </div>
+                    <div className="mainWrapper">
+                        <div className="mainContent">
+                            <div className="imageHolder">
+                                {imgURL && <img src={imgURL} alt="preview" crossOrigin='anonymous' ref={imageRef} />}
+                            </div>
+                            {results.length > 0 && <div className='resultsHolder'>
+                                {results.map((result, index) => {
+                                    return (
+                                        <div className="result" key={result.className}>
+                                            <span className="name">{result.className}</span>
+                                            <span className="confidence">Confidence level: {(result.probability * 100).toFixed(2)}%
+                                                {index === 0 && <span className='bestGuess'>Best Guess</span>}
+                                            </span>
+                                        </div>
+                                    )
+                                })}
+                            </div>}
+                        </div>
+                        {imgURL && <button className='button' onClick={identify}>Identify image</button>}
+                    </div>
+                </div>
+                {showAlert && <AlertErrorImages />}
+            </>
+        );
     }
 
-    return (
-        <>
-            <div className="App">
-                <div className="inputHolder">
-                    <input type='file' accept='image/*' capture='camera' className='uploadInput' onChange={uploadImage} ref={fileInputRef} />
-                    <button className='uploadImage' onClick={triggerUpload}>Upload Image</button>
-                    <span className="or">OR</span>
-                    <input type="text" placeholder='Enter URL image...' ref={textInputRef} onChange={handleOnChange} />
-                </div>
-                <div className="mainWrapper">
-                    <div className="mainContent">
-                        <div className="imageHolder">
-                            {imgURL && <img src={imgURL} alt="preview" crossOrigin='anonymous' ref={imageRef} />}
-                        </div>
-                        {results.length > 0 && <div className='resultsHolder'>
-                            {results.map((result, index) => {
-                                return (
-                                    <div className="result" key={result.className}>
-                                        <span className="name">{result.className}</span>
-                                        <span className="confidence">Confidence level: {(result.probability * 100).toFixed(2)}%
-                                            {index === 0 && <span className='bestGuess'>Best Guess</span>}
-                                        </span>
-                                    </div>
-                                )
-                            })}
-                        </div>}
-                    </div>
-                    {imgURL && <button className='button' onClick={identify}>Identify image</button>}
-                </div>
-            </div>
-        </>
-    );
 }
 
 export default ButtonImages;
